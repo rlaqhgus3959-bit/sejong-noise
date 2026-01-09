c8076d094f23c6f7197d5f85ff85fd817476851f
rlaqhgus3959-bit <rlaqhgus3959@gmail.com>
2026-01-09 10:08:57 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+﻿const CACHE_NAME = '소음계산기-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
